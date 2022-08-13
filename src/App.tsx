1fa6fc4c268c3914f72a532b9d07f5ce8bc96989
Alexander Bryakin <alexandebryakin@gmail.com>
2022-08-13 18:36:17 +0300

    feat: add Login page markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import 'antd/dist/antd.css';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Login from './pages/Login/Login.component';
+import Signup from './pages/Signup/Signup.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          // element={<App />}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {/* <Route index element={<Home />} /> */}
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
